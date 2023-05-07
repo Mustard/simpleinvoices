778430060d2fe83ce2cda6b7dc0aff6b7ff87a58
Daniel Mabbett <triumph_2500@hotmail.com>
2023-05-07 16:10:10 +1000

    Simplify smarty template

--- include/smarty_plugins/function.do_tr.php
+++ include/smarty_plugins/function.do_tr.php
@@ -2,17 +2,9 @@
 
 function smarty_function_do_tr($params, &$smarty)
 {
-	if ($params['number'] == 2 ) {
-		$new_tr = "</tr><tr class='$params[class]'>";
-		return $new_tr;
-	}
-	
-        if ($params['number'] == 4 ) {
-                $new_tr = "</tr><tr class='$params[class]'>";
-                return $new_tr;
-        }
+    if ($params['number'] % 2 == 0) {
+        return "</tr><tr class='$params[class]'>";
+    }
 
 }
 
-
-?>
